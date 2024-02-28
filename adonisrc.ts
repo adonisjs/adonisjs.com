afb7ff74d3c81a7ee5cf49bc56fbfa87e04517bd
Julien Ripouteau <julien@ripouteau.com>
2024-02-28 01:06:15 +0100

    refactor: use new vite version

--- adonisrc.ts
+++ adonisrc.ts
@@ -80,4 +80,9 @@ export default defineConfig({
       reloadServer: false,
     },
   ],
+
+  assetsBundler: false,
+  unstable_assembler: {
+    onBuildStarting: [() => import('@adonisjs/vite/build_hook')],
+  },
 })
