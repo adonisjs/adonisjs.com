a4632f6aa7909b0f1334aca0805905c48ee8f325
Matías Surdi <matiassurdi@gmail.com>
2024-10-23 17:25:36 +0200

    Limit the number of posts in the feed

--- app/controllers/blog_feed_controller.ts
+++ app/controllers/blog_feed_controller.ts
@@ -2,11 +2,16 @@ import type { HttpContext } from '@adonisjs/core/http'
 import BlogPosts from '../collections/blog_posts.js'
 
 export default class BlogFeedController {
-  handle({ view, response }: HttpContext) {
+  async handle({ view, response }: HttpContext) {
     response.append('Content-Type', 'text/xml')
 
+    const publishedBlogPosts = await new BlogPosts().published()
+
+    // Only publish the most recent 20 posts
+    const feedBlogPosts = publishedBlogPosts.slice(0, 20)
+
     return view.render('feeds/blog', {
-      blogPosts: new BlogPosts(),
+      blogPosts: feedBlogPosts,
     })
   }
 }
